71587c6171f4be7397d43b52d57521436d456063
Sergio Huapaya Llimper <sergio.huapaya0704@gmail.com>
2025-04-27 21:39:55 -0500

    Guarde los cambios de la clase Login

--- JavaProject_Waybus/src/Proyecto/Login.java
+++ JavaProject_Waybus/src/Proyecto/Login.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Proyecto;
-
 /**
- *
  * @author sergi
  */
 public class Login {
+    private String usuario;
+    private String contraseña;
+
+    public Login(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+    public Login(){
+        
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
     
 }
