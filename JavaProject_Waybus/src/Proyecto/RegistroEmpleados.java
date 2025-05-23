6a0009338709227041abf2f95185217882a70115
Shuu <franckyc2020@gmail.com>
2025-05-22 22:03:12 -0500

    cambios locales

---
+++ JavaProject_Waybus/src/Proyecto/RegistroEmpleados.java
@@ -0,0 +1,53 @@
+package Proyecto;
+
+public class RegistroEmpleados extends Persona {
+    private String codigo;
+    private String oficina;
+    private String cargo;
+
+    public RegistroEmpleados() {}
+
+    // Constructor con todos los datos en el mismo orden del archivo
+    public RegistroEmpleados(
+        String codigo,
+        String nombres,
+        String apellidos,
+        String dni,
+        String correo,
+        String telefono,
+        String oficina,
+        String cargo,
+        String sexo,
+        int edad
+    ) {
+        super(nombres, apellidos, dni, correo, telefono, edad, sexo);
+        this.codigo = codigo;
+        this.oficina = oficina;
+        this.cargo = cargo;
+    }
+
+    // Getters y Setters propios
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getOficina() {
+        return oficina;
+    }
+
+    public void setOficina(String oficina) {
+        this.oficina = oficina;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
