59dd0426ef41cf8ea24c8904736fad46b2cc704d
Shuu <franckyc2020@gmail.com>
2025-05-27 01:22:51 -0500

    Arreglo de clase empleados

--- JavaProject_Waybus/src/Proyecto/RegistroEmpleados.java
+++ JavaProject_Waybus/src/Proyecto/RegistroEmpleados.java
@@ -8,7 +8,7 @@ public class RegistroEmpleados extends Persona {
     public RegistroEmpleados() {}
 
     // Constructor con todos los datos en el mismo orden del archivo
-    public RegistroEmpleados(String codigo, String oficina, String cargo, String nombres, String apellidos, String dni, String correo, String telefono, int edad, String sexo) {
+    public RegistroEmpleados(String codigo, String nombres, String apellidos, String dni, String correo, String telefono, String oficina, String cargo, int edad, String sexo ) {
         super(nombres, apellidos, dni, correo, telefono, edad, sexo);
         this.codigo = codigo;
         this.oficina = oficina;
